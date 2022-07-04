cc597f6cddbdbaf0b2a7de3d1e15e7d0676a6a5b
Andy Bigler <andy.bigler@swisscom.com>
2022-07-04 11:13:59 +0200

    Command

---
+++ src/Behavioral_Patterns/Command/Main.java
@@ -0,0 +1,10 @@
+package Behavioral_Patterns.Command;
+
+import Behavioral_Patterns.Command.editor.Editor;
+
+public class Main {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
