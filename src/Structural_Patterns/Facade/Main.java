8010f7c36ff9bd78c0881c426023a28a6cfb95a1
Jan Lauber <jan.lauber@protonmail.ch>
2022-07-04 10:56:53 +0200

    add facade pattern
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

---
+++ src/Structural_Patterns/Facade/Main.java
@@ -0,0 +1,13 @@
+package Structural_Patterns.Facade;
+
+import Structural_Patterns.Facade.facade.VideoConversionFacade;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+        // ...
+    }
+}
---
+++ src/Structural_Patterns/Facade/some_complex_media_library/Codec.java
@@ -0,0 +1,4 @@
+package Structural_Patterns.Facade.some_complex_media_library;
+
+public interface Codec {
+}
